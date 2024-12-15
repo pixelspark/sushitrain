b92f4b230c4165869fb90f3ac35b49d4cc016061
Tommy van der Vorst <tommy@pixelspark.nl>
2024-12-15 12:26:12 +0100

    feat: add 'soft clean' that only removes local copies for files that are not currently available on at least one other peer

--- Sushitrain/SelectiveFolderView.swift
+++ Sushitrain/SelectiveFolderView.swift
@@ -16,7 +16,8 @@ struct SelectiveFolderView: View {
     @State private var isLoading = true
     @State private var isClearing = false
     @State private var selectedPaths: [String] = []
-    @State private var showConfirmClearSelection = false
+    @State private var showConfirmClearSelectionHard = false
+    @State private var showConfirmClearSelectionSoft = false
     
     var body: some View {
         Group {
@@ -47,27 +48,53 @@ struct SelectiveFolderView: View {
                 ContentUnavailableView("No files selected", systemImage: "pin.slash.fill", description: Text("To keep files on this device, navigate to a file and select 'keep on this device'. Selected files will appear here."))
             }
         }
-        
         .toolbar {
             ToolbarItem(placement: .primaryAction) {
-                Button("Free up space", systemImage: "pin.slash", action: {
-                    showConfirmClearSelection = true
-                }).disabled(isClearing || isLoading)
-                .help("Remove the files shown in the list from this device, but do not remove them from other devices.")
+                Menu {
+                    Section("Remove files from this device") {
+                        Button("All files that are available on other devices", systemImage: "pin.slash", action: {
+                            showConfirmClearSelectionSoft = true
+                        })
+                        .help("Remove the files shown in the list from this device, but do not remove them from other devices. If a file is not available on at least one other device, it will not be removed.")
+                        
+                        Button("All files including those not available elsewhere", systemImage: "pin.slash", action: {
+                            showConfirmClearSelectionHard = true
+                        })
+                        .help("Remove the files shown in the list from this device, but do not remove them from other devices. If the file is not available on another device it will be permanently removed.")
+                    }
+                }
+                label: {
+                   Label("Free up space", systemImage:  "pin.slash")
+               }.disabled(isClearing || isLoading)
             }
         }
-        .alert(isPresented: $showConfirmClearSelection) {
-            Alert(
-                title: Text("Free up space"),
-                message: Text("This will remove all locally stored copies of files in this folder. Any files that are not also present on another device will be permanently lost and cannot be recovered. Are you sure yu want to continue?"),
-                primaryButton: .destructive(Text("Remove files")) {
-                    showConfirmClearSelection = false
-                    self.clearSelection()
-                },
-                secondaryButton: .cancel() {
-                    showConfirmClearSelection = false
-                }
-            )
+        .alert(isPresented: .constant(showConfirmClearSelectionSoft || showConfirmClearSelectionHard)) {
+            if showConfirmClearSelectionSoft {
+                return Alert(
+                    title: Text("Free up space"),
+                    message: Text("This will remove all locally stored copies of files in this folder. Any files that are not also present on another device will not be removed. Are you sure yu want to continue?"),
+                    primaryButton: .destructive(Text("Remove files")) {
+                        showConfirmClearSelectionSoft = false
+                        self.clearSelectionSoft()
+                    },
+                    secondaryButton: .cancel() {
+                        showConfirmClearSelectionSoft = false
+                    }
+                )
+            }
+            else {
+                return Alert(
+                    title: Text("Free up space"),
+                    message: Text("This will remove all locally stored copies of files in this folder. Any files that are not also present on another device will be permanently lost and cannot be recovered. Are you sure yu want to continue?"),
+                    primaryButton: .destructive(Text("Remove files")) {
+                        showConfirmClearSelectionHard = false
+                        self.clearSelectionHard()
+                    },
+                    secondaryButton: .cancel() {
+                        showConfirmClearSelectionHard = false
+                    }
+                )
+            }
         }
         .navigationBarBackButtonHidden(isClearing)
         
@@ -86,9 +113,38 @@ struct SelectiveFolderView: View {
         }
     }
     
-    private func clearSelection() {
-        if searchString.isEmpty && !isClearing {
-            isClearing = true
+    @MainActor
+    private func clearSelectionSoft() {
+        if isClearing {
+            return
+        }
+        isClearing = true
+        
+        Task.detached {
+            do {
+                try await self.deselectSearchResults(includingLastCopy: false)
+            }
+            catch let error {
+                DispatchQueue.main.async {
+                    showError = true
+                    errorText = error.localizedDescription
+                }
+            }
+            DispatchQueue.main.async {
+                self.update()
+                isClearing = false
+            }
+        }
+    }
+    
+    @MainActor
+    private func clearSelectionHard() {
+        if isClearing {
+            return
+        }
+        isClearing = true
+        
+        if searchString.isEmpty {
             Task.detached {
                 do {
                     try folder.clearSelection()
@@ -106,7 +162,21 @@ struct SelectiveFolderView: View {
             self.selectedPaths.removeAll()
         }
         else {
-            self.deselectSearchResults()
+            Task.detached {
+                do {
+                    try await self.deselectSearchResults(includingLastCopy: true)
+                }
+                catch {
+                    DispatchQueue.main.async {
+                        errorText = error.localizedDescription
+                        showError = true
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.update()
+                    isClearing = false
+                }
+            }
         }
     }
     
@@ -123,22 +193,32 @@ struct SelectiveFolderView: View {
         self.isLoading = false
     }
     
-    private func deselectSearchResults() {
-        do {
-            let st = searchString.lowercased()
-            let verdicts = self.selectedPaths.filter { item in
-                return st.isEmpty || item.lowercased().contains(st)
-            }.reduce(into: [:]) { dict, p in
-                dict[p] = false
-            }
-            let json = try JSONEncoder().encode(verdicts)
-            try folder.setExplicitlySelectedJSON(json)
-            Task {
-                self.update()
+    private func deselectSearchResults(includingLastCopy: Bool) async throws {
+        let st = searchString.lowercased()
+        
+        var paths = Set(self.selectedPaths.filter { item in
+            return st.isEmpty || item.lowercased().contains(st)
+        })
+        
+        // If we should not delete if our copy is the only one available, check availability for each file first
+        if !includingLastCopy {
+            for path in paths {
+                let entry = try folder.getFileInformation(path)
+                let peersWithFullCopy = try entry.peersWithFullCopy()
+                if peersWithFullCopy.count() == 0 {
+                    Log.info("Not removing \(path), it is the last copy")
+                    paths.remove(path)
+                }
             }
         }
-        catch {
-            Log.warn("Could not deselect: \(error.localizedDescription)")
+        
+        let verdicts = paths.reduce(into: [:]) { dict, p in
+            dict[p] = false
+        }
+        let json = try JSONEncoder().encode(verdicts)
+        try folder.setExplicitlySelectedJSON(json)
+        Task {
+            self.update()
         }
     }
     
@@ -178,14 +258,21 @@ fileprivate struct SelectiveFileView: View {
                                 
                                 Spacer()
                                 
-                                if !fa.isEmpty {
-                                    Button("Remove from this device, keep on \(fa.count) others", systemImage: "pin.slash") {
-                                        self.deselect()
-                                    }
-                                    .labelStyle(.iconOnly)
-                                    .foregroundStyle(.red)
-                                    .buttonStyle(.borderless)
+                                if fa.isEmpty {
+                                    Text("Only copy")
+                                        .foregroundStyle(.orange).bold().padding(.all, 3).overlay(
+                                            RoundedRectangle(cornerRadius: 3).stroke(Color.orange, lineWidth: 1)
+                                        )
+                                        .help("This file is only available on this device.")
                                 }
+                                
+                                Button("Remove from this device, keep on \(fa.count) others", systemImage: "pin.slash") {
+                                    self.deselect()
+                                }
+                                .disabled(fa.isEmpty)
+                                .labelStyle(.iconOnly)
+                                .foregroundStyle(fa.isEmpty ? .gray : .red)
+                                .buttonStyle(.borderless)
                             }
                             else {
                                 Label(path, systemImage: file.systemImage)
