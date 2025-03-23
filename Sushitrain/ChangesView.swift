574ea8cfa6eaee0ab934de54e7d9c90ede5127f5
Tommy van der Vorst <tommy@pixelspark.nl>
2025-03-23 21:40:48 +0100

    ux: allow navigating to file from recent changes view

--- Sushitrain/ChangesView.swift
+++ Sushitrain/ChangesView.swift
@@ -5,41 +5,54 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 import Foundation
 import SwiftUI
+@preconcurrency import SushitrainCore
 
 struct ChangesView: View {
 	@ObservedObject var appState: AppState
+
 	var body: some View {
 		List {
 			ForEach(appState.lastChanges, id: \.id) { change in
 				if let folder = appState.client.folder(withID: change.folderID) {
-					HStack {
-						VStack(alignment: .leading) {
-							Text("\(folder.displayName): \(change.path)")
-								.multilineTextAlignment(.leading)
-								.bold()
-
-							if let dateString = change.time?.date().formatted() {
-								Text(dateString).dynamicTypeSize(.small)
-									.foregroundColor(.gray)
-							}
-
-							if let peer = appState.client.peer(withShortID: change.shortID) {
-								if peer.deviceID() == appState.localDeviceID {
-									Text("By this device").dynamicTypeSize(.small)
-										.foregroundColor(.gray)
-								}
-								else {
-									Text("By \(peer.displayName)").dynamicTypeSize(
-										.small
-									).foregroundColor(.gray)
-								}
-							}
-						}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-						Image(systemName: change.systemImage)
+					if let entry = try? folder.getFileInformation(change.path), !entry.isDeleted() {
+						NavigationLink(destination: FileView(file: entry, appState: self.appState, showPath: true, siblings: [])) {
+							self.changeDetails(change: change, folder: folder)
+						}
+					}
+					else {
+						self.changeDetails(change: change, folder: folder)
 					}
 				}
 			}
 		}
 		.navigationTitle("Recent changes")
+	}
+
+	@ViewBuilder private func changeDetails(change: SushitrainChange, folder: SushitrainFolder) -> some View {
+		HStack {
+			VStack(alignment: .leading) {
+				Text("\(folder.displayName): \(change.path)")
+					.multilineTextAlignment(.leading)
+					.bold()
+
+				if let dateString = change.time?.date().formatted() {
+					Text(dateString).dynamicTypeSize(.small)
+						.foregroundColor(.gray)
+				}
+
+				if let peer = appState.client.peer(withShortID: change.shortID) {
+					if peer.deviceID() == appState.localDeviceID {
+						Text("By this device").dynamicTypeSize(.small)
+							.foregroundColor(.gray)
+					}
+					else {
+						Text("By \(peer.displayName)").dynamicTypeSize(
+							.small
+						).foregroundColor(.gray)
+					}
+				}
+			}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+			Image(systemName: change.systemImage)
+		}
 	}
 }
