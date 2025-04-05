89c86acbe1975be6dd154dbd19b9297b6b762168
Tommy van der Vorst <tommy@pixelspark.nl>
2025-04-05 09:05:29 +0200

    fix: disable idle timer while migrating (fixes #181)

--- Sushitrain/AppState.swift
+++ Sushitrain/AppState.swift
@@ -160,7 +160,7 @@ enum AppStartupState: Equatable {
 		if resetDeltas {
 			Log.info("Reset deltas requested from settings")
 		}
-		
+
 		DispatchQueue.global(qos: .userInitiated).async {
 			do {
 				// This one opens the database, migrates stuff, etc. and may take a while
@@ -176,7 +176,7 @@ enum AppStartupState: Equatable {
 					self.isMigratedToNewDatabase = !client.hasOldDatabase()
 					Log.info("Performing app migrations...")
 					self.performMigrations()
-					
+
 					Log.info("Configuring the user interface...")
 					self.applySettings()
 					self.update()
