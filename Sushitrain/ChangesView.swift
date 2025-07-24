9a2b745eef7fff3f040159b3aecd19b092cd7933
Tommy van der Vorst <tommy@pixelspark.nl>
2025-07-24 21:09:52 +0200

    ui: show recent changes in a table on macOS, improve performance

--- Sushitrain/ChangesView.swift
+++ Sushitrain/ChangesView.swift
@@ -8,24 +8,127 @@ import SwiftUI
 @preconcurrency import SushitrainCore
 
 struct ChangesView: View {
+	private struct ChangeInfo: Identifiable, Hashable {
+		var id: ObjectIdentifier {
+			return change.id
+		}
+
+		var change: SushitrainChange
+		var entry: SushitrainEntry?
+		var folder: SushitrainFolder?
+		var peer: SushitrainPeer?
+	}
+
+	@State private var changes: [ChangeInfo] = []
 	@Environment(AppState.self) private var appState
 
+	#if os(macOS)
+		@State private var inspectedChange: ChangeInfo? = nil
+		@State private var selectedChanges = Set<ChangeInfo.ID>()
+		@SceneStorage("ChangesTableViewConfig") private var columnCustomization: TableColumnCustomization<ChangeInfo>
+	#endif
+
 	var body: some View {
-		List {
-			ForEach(appState.lastChanges, id: \.id) { change in
-				if let folder = appState.client.folder(withID: change.folderID) {
-					if let entry = try? folder.getFileInformation(change.path), !entry.isDeleted() {
-						NavigationLink(destination: FileView(file: entry, showPath: true, siblings: nil)) {
-							self.changeDetails(change: change, folder: folder)
+		#if os(macOS)
+			Table(changes, selection: $selectedChanges, columnCustomization: $columnCustomization) {
+				TableColumn("") { change in
+					Image(systemName: change.change.systemImage)
+				}.width(min: 32, max: 32).customizationID("icon")
+
+				TableColumn("Folder") { change in
+					Text(change.folder?.displayName ?? "")
+				}.width(ideal: 100).customizationID("folder")
+
+				TableColumn("File") { change in
+					Text(change.change.path)
+				}.customizationID("file")
+
+				TableColumn("Changed at") { change in
+					if let dateString = change.change.time?.date().formatted() {
+						Text(dateString)
+					}
+				}.width(ideal: 100).customizationID("date")
+
+				TableColumn("Changed by") { change in
+					if let peer = change.peer {
+						if peer.deviceID() == appState.localDeviceID {
+							Text("This device")
+						}
+						else {
+							Text(peer.displayName)
 						}
 					}
-					else {
-						self.changeDetails(change: change, folder: folder)
+				}.width(ideal: 100).customizationID("changingDevice")
+			}
+			.contextMenu(
+				forSelectionType: ChangeInfo.ID.self,
+				menu: { items in
+					Text("\(items.count) selected")
+				}, primaryAction: self.doubleClick
+			)
+			.task {
+				await self.update()
+			}
+			.onChange(of: appState.lastChanges) { _, _ in
+				Task {
+					await self.update()
+				}
+			}
+			.navigationTitle("Recent changes")
+			.navigationDestination(item: $inspectedChange) { change in
+				if let entry = change.entry {
+					FileView(file: entry, showPath: true, siblings: nil)
+				}
+			}
+		#else
+			List {
+				ForEach(changes, id: \.id) { change in
+					if let folder = change.folder {
+						if let entry = change.entry {
+							NavigationLink(destination: FileView(file: entry, showPath: true, siblings: nil)) {
+								self.changeDetails(change: change.change, folder: folder)
+							}
+						}
+						else {
+							self.changeDetails(change: change.change, folder: folder)
+						}
 					}
 				}
 			}
+			.task {
+				await self.update()
+			}
+			.onChange(of: appState.lastChanges) { _, _ in
+				Task {
+					await self.update()
+				}
+			}
+			.navigationTitle("Recent changes")
+		#endif
+	}
+
+	#if os(macOS)
+		private func doubleClick(_ items: Set<ChangeInfo.ID>) {
+			if let itemID = items.first, items.count == 1 {
+				if let change = self.changes.first(where: { $0.id == itemID }) {
+					self.inspectedChange = change
+				}
+			}
 		}
-		.navigationTitle("Recent changes")
+	#endif
+
+	private func update() async {
+		self.changes = appState.lastChanges.map { change in
+			let folder = appState.client.folder(withID: change.folderID)
+			let entry = try? folder?.getFileInformation(change.path)
+			let peer = appState.client.peer(withShortID: change.shortID)
+			return ChangeInfo(
+				change: change,
+				entry: entry,
+				folder: folder,
+				peer: peer
+			)
+		}
 	}
 
 	@ViewBuilder private func changeDetails(change: SushitrainChange, folder: SushitrainFolder) -> some View {
