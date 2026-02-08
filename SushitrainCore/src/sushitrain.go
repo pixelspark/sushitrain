bd7332c757a91c74dbf36d35988314e9719e776d
Tommy van der Vorst <tommy@pixelspark.nl>
2026-02-08 09:22:23 +0100

    chore: update Syncthing to 2.0.14
    Signed-off-by: Tommy van der Vorst <tommy@pixelspark.nl>

--- SushitrainCore/src/sushitrain.go
+++ SushitrainCore/src/sushitrain.go
@@ -110,7 +110,7 @@ const (
 
 func NewClient(configPath string, filesPath string, saveLog bool) *Client {
 	// Set version info
-	build.Version = "v2.0.13"
+	build.Version = "v2.0.14"
 	build.Host = "t-shaped.nl"
 	build.User = "sushitrain"
 
