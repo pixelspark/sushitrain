c8455fea891ab3ed93037e9cce3c718c72fab981
Tommy van der Vorst <tommy@pixelspark.nl>
2026-05-09 11:04:59 +0200

    ux: refer to the pattern docs in the ignore patterns screen
    Signed-off-by: Tommy van der Vorst <tommy@pixelspark.nl>

--- Sushitrain/IgnoresView.swift
+++ Sushitrain/IgnoresView.swift
@@ -75,7 +75,7 @@ struct IgnoresView: View {
 				Text("Ignore patterns")
 			} footer: {
 				Text(
-					"Files and subdirectories whose paths match any of the patterns above will not be synchronized with other devices. Existing items matching the patterns will not be updated."
+					"Files and subdirectories whose paths match any of the patterns above will not be synchronized with other devices. Existing items matching the patterns will not be updated. [Learn more about the pattern syntax...](https://docs.syncthing.net/users/ignoring.html#patterns)"
 				)
 				#if os(macOS)
 					.lineLimit(3, reservesSpace: true)
@@ -275,7 +275,7 @@ struct SelectiveIgnoresView: View {
 					#endif
 			} footer: {
 				Text(
-					"Files and subdirectories whose paths match any of the patterns above will not be synchronized with other devices. Existing items matching the patterns will not be updated."
+					"Files and subdirectories whose paths match any of the patterns above will not be synchronized with other devices. Existing items matching the patterns will not be updated. [Learn more about the pattern syntax...](https://docs.syncthing.net/users/ignoring.html#patterns)"
 				)
 				#if os(macOS)
 					.lineLimit(3, reservesSpace: true)
